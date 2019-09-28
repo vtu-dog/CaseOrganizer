bce45bcb3908f73dcfc419f9b6e9cecf08f7f468
Dawid Gałka <idsbdofficial@gmail.com>
2019-09-28 23:12:29 +0200

    Adding GUI.fxml

---
+++ src/main/java/CaseOrganizer/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
+
+        stage.setTitle("Zarządca postępowań");
+        stage.setScene(new Scene(root, 500, 500));
+        stage.show();
+    }
+}
